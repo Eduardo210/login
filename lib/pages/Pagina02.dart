1dff57288333a91be593d75d4d001be0ff709a49
JaimeAcciona <jaime.sanchez@accionaingenieria.com.mx>
2021-05-31 10:24:01 -0500

    Pagina02

--- lib/pages/Pagina02.dart
+++ lib/pages/Pagina02.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
 class Pagina02 extends StatelessWidget {
+  final opciones = ['Archivos', 'Imágenes', 'Contactos', 'Perfil', 'Pagos'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:  AppBar(
-        title: Text("Terminos y condiciones"),
+      appBar: AppBar(
+        title: Text("Menú"),
       ),
+      body: ListView(children: _crearMenu()),
     );
+  }
+
+  List<Widget> _crearMenu() {
+    return opciones
+        .map((item) => Column(
+              children: [
+                ListTile(
+                  title: Text(item + ':'),
+                  leading: Icon(Icons.menu),
+                  trailing: Icon(Icons.keyboard_arrow_right),
+                  onTap: () {},
+                ),
+                Divider()
+              ],
+            ))
+        .toList();
   }
 }
